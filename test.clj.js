5040a183101707041dc798a78ae9043b847c80ab
Oliver Fencott <oliverfencott@gmail.com>
2022-01-13 06:48:16 +0000

    Remove output file

--- test.clj.js
+++
@@ -1,9 +0,0 @@
-function make__HYPHEN__name(name) {
-  return js__HYPHEN__string__HYPHEN__concat("hello, ", name, "!");
-}
-
-module.exports = {
-  make_name: function (name) {
-    return make__HYPHEN__name(name);
-  },
-};
